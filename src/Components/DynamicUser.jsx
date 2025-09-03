a2a1cad8e92d1627c49788b2bf6992d1a67ec43e
Nur Alam Shawon <ahsan.shawon9@gmail.com>
2025-09-03 09:26:01 +0600

    partial completed

--- src/Components/DynamicUser.jsx
+++ src/Components/DynamicUser.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 const DynamicUser = () => {
   const data = useLoaderData();
   return (
@@ -16,9 +18,14 @@ const DynamicUser = () => {
       </div>
 
       {/* Main Content */}
-      <div className="grid grid-cols-2 justify-between space-x-8">
+      <div className="grid grid-cols-1 xl:grid-cols-2 justify-between xl:space-x-8">
         {/* Left Column: Personal Information */}
-        <div className="col-span- bg-gray-100 p-4 rounded-xl">
+        <div
+          className="col-span-1 bg-gray-100 p-4 rounded-xl xl:mb-0 mb-8"
+          data-aos="fade-left"
+          data-aos-delay="200"
+          data-aos-anchor=".example-selector"
+        >
           <h3 className="text-xl font-semibold text-gray-800 mb-4">
             Personal Information
           </h3>
@@ -35,32 +42,42 @@ const DynamicUser = () => {
             <p className="font-semibold text-gray-800">{data.phone}</p>
 
             <p className="text-gray-500">Website</p>
-            <a className="font-semibold text-blue-800" href={`https://${data.website}`}>{data.website}</a>
+            <a
+              className="font-semibold text-blue-800"
+              href={`https://${data.website}`}
+            >
+              {data.website}
+            </a>
           </div>
         </div>
 
         {/* Right Column: Address */}
-          <div className="col-span- bg-gray-100 p-4 rounded-xl">
+        <div
+          className="col-span-1 bg-gray-100 p-4 rounded-xl"
+          data-aos="fade-left"
+          data-aos-delay="200"
+          data-aos-anchor=".example-selector"
+        >
           <h3 className="text-xl font-semibold text-gray-800 mb-4">Address</h3>
           <div className="space-y-2">
-           
-              <p className="text-gray-500">Street</p>
-              <p className="font-semibold text-gray-800">{data.address.street}</p>
-            
-              <p className="text-gray-500">Suite</p>
-              <p className="font-semibold text-gray-800">{data.address.suite}</p>
-            
-              <p className="text-gray-500">City</p>
-              <p className="font-semibold text-gray-800">{data.address.city}</p>
-           
-              <p className="text-gray-500">Zipcode</p>
-              <p className="font-semibold text-gray-800">9{data.address.zipcode}874</p>
-            
-              <p className="text-gray-500">Geo Location</p>
-              <p className="font-semibold text-gray-800">
-               {data.address.geo.lat} , {data.address.geo.lng}
-              </p>
-          
+            <p className="text-gray-500">Street</p>
+            <p className="font-semibold text-gray-800">{data.address.street}</p>
+
+            <p className="text-gray-500">Suite</p>
+            <p className="font-semibold text-gray-800">{data.address.suite}</p>
+
+            <p className="text-gray-500">City</p>
+            <p className="font-semibold text-gray-800">{data.address.city}</p>
+
+            <p className="text-gray-500">Zipcode</p>
+            <p className="font-semibold text-gray-800">
+              9{data.address.zipcode}874
+            </p>
+
+            <p className="text-gray-500">Geo Location</p>
+            <p className="font-semibold text-gray-800">
+              {data.address.geo.lat} , {data.address.geo.lng}
+            </p>
           </div>
         </div>
       </div>
@@ -68,7 +85,12 @@ const DynamicUser = () => {
       {/* Company Details */}
       <div className="mt-8 bg-gray-100 p-4  rounded-xl">
         <h3 className="text-xl font-semibold text-gray-800 mb-4">Company</h3>
-        <div className="grid grid-cols-3  ">
+        <div
+          className="grid grid-cols-1 xl:grid-cols-3 "
+          data-aos="fade-left"
+          data-aos-delay="200"
+          data-aos-anchor=".example-selector"
+        >
           <div>
             <p className="text-gray-500">Company Name</p>
             <p className="font-semibold text-gray-800">{data.company.name}</p>
@@ -81,9 +103,7 @@ const DynamicUser = () => {
           </div>
           <div>
             <p className="text-gray-500">Business</p>
-            <p className="font-semibold text-gray-800">
-             {data.company.bs}
-            </p>
+            <p className="font-semibold text-gray-800">{data.company.bs}</p>
           </div>
         </div>
       </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,17 @@
+import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 const Home = () => {
   const [users, setUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredUsers, setFilteredUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(5);
+  const [loading, setLoading] = useState(false); // 👈 Full page loading state
 
-  // Fetch users from the API
   useEffect(() => {
     const fetchUser = async () => {
       const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -31,65 +37,126 @@ const Home = () => {
       );
       setFilteredUsers(results);
     }
+    setCurrentPage(1);
+  };
+
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+
+  const nextPage = () => {
+    if (currentPage < Math.ceil(filteredUsers.length / usersPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleRowClick = (id) => {
+    setLoading(true);
+    setTimeout(() => {
+      window.location.href = `/description/${id}`;
+    }, 1000);
   };
 
   return (
-    <div className="xl:max-w-7xl p-6 rounded-xl shadow-xl mx-auto bg-white space-y-6">
-      <h1 className="text-2xl font-bold">User Management</h1>
-      <div className="flex items-center">
-        <label className="input rounded-lg w-276">
-          <input
-            type="search"
-            required
-            placeholder="Search by Name or Email"
-            value={searchQuery}
-            onChange={handleSearchChange}
-            className="placeholder-gray-100"
+    <div className="relative">
+      {/* Full page loading overlay */}
+      {loading && (
+        <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
+          <motion.div
+            className="w-14 h-14 border-4 border-blue-600 border-t-transparent rounded-full"
+            animate={{ rotate: 360 }}
+            transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
           />
-        </label>
-        <button
-          className="btn ml-4 text-white bg-blue-600 rounded-xl w-23 join-item"
-          onClick={handleSearchClick}
-        >
-          Search
-        </button>
-      </div>
+        </div>
+      )}
 
-      <div className="overflow-x-auto">
-        <table className="table-auto w-full">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-4 py-2 text-left">Name</th>
-              <th className="px-4 py-2 text-left">Email</th>
-              <th className="px-4 py-2 text-left">Phone</th>
-              <th className="px-4 py-2 text-left">Company</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredUsers.length > 0 ? (
-              filteredUsers.map((user) => (
-                <tr
-                  key={user.id}
-                  className="border-b-1 border-gray-300 min-h-15 hover:bg-gray-100 cursor-pointer"
-                  onClick={() =>
-                    (window.location.href = `/description/${user.id}`)
-                  }
-                >
-                  <td className="px-4 py-4">{user.name}</td>
-                  <td className="px-4 py-4">{user.email}</td>
-                  <td className="px-4 py-4">{user.phone}</td>
-                  <td className="px-4 py-4">{user.company.name}</td>
-                </tr>
-              ))
-            ) : (
+      {/* Main content */}
+      <div className="xl:max-w-7xl p-6 rounded-xl shadow-xl mx-auto bg-white space-y-6">
+        <h1 className="text-2xl font-bold">User Management</h1>
+        <div className="xl:flex flex-row items-center">
+          <label className="input rounded-lg xl:w-276 w-full">
+            <input
+              type="search"
+              required
+              placeholder="Search by Name or Email"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="placeholder-gray-100 "
+            />
+          </label>
+          <button
+            className="btn xl:ml-4 xl:mt-0 mt-1 text-white bg-blue-600 rounded-xl xl:w-23 w-full join-item"
+            onClick={handleSearchClick}
+          >
+            Search
+          </button>
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full">
+            <thead className="bg-gray-50">
               <tr>
-                <td colSpan="4" className="px-4 py-2 text-center">
-                  No results found
-                </td>
+                <th className="px-4 py-2 text-left">Name</th>
+                <th className="px-4 py-2 text-left">Email</th>
+                <th className="px-4 py-2 text-left">Phone</th>
+                <th className="px-4 py-2 text-left">Company</th>
               </tr>
-            )}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {currentUsers.length > 0 ? (
+                currentUsers.map((user) => (
+                  <tr
+                    key={user.id}
+                    className="border-b-1 border-gray-300 min-h-15 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => handleRowClick(user.id)}
+                     data-aos="fade-left" data-aos-delay="100" data-aos-anchor=".example-selector"
+                  >
+                    <td className="px-4 py-4">{user.name}</td>
+                    <td className="px-4 py-4">{user.email}</td>
+                    <td className="px-4 py-4">{user.phone}</td>
+                    <td className="px-4 py-4">{user.company.name}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4" className="px-4 py-2 text-center">
+                    No results found
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Pagination */}
+        <div className="mt-6 flex justify-between items-center">
+          <button
+            className="text-sm bg-gray-300 p-2 rounded-lg"
+            onClick={prevPage}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <span className="text-sm text-gray-600">
+            Page {currentPage} of{" "}
+            {Math.ceil(filteredUsers.length / usersPerPage)}
+          </span>
+          <button
+            className="text-sm bg-gray-300 p-2 rounded-lg"
+            onClick={nextPage}
+            disabled={
+              currentPage === Math.ceil(filteredUsers.length / usersPerPage)
+            }
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
